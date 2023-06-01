a756dca877d26f5468e268b71403b2a8f3de47ee
risoocto <134274209+risoocto@users.noreply.github.com>
2023-06-01 14:44:46 +0200

    Update streamlit_app.py
    added icones

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('My parents New Healthy Diner')
-streamlit.text('Hard-Boile Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boile Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
